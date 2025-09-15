55234bc34666be9ca9caa6f471db187c233467a3
Tony Abuya <tonyowuor22@gmail.com>
2025-09-15 20:02:46 +0300

    Calculate monthly savings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,3 +1,5 @@
 monthly_income = float(input("Enter your monthly income: "))
 monthly_expenses = float(input("Enter your total monthly expenses: "))
 
+monthly_savings = monthly_income - monthly_expenses
+
