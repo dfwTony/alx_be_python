f1121183e6ae9090d9c42c67e43d91b0c0855f25
Tony Abuya <tonyowuor22@gmail.com>
2025-09-15 20:04:44 +0300

    Display monthly savings and projected annual savings with interest

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,3 +5,5 @@ monthly_savings = monthly_income - monthly_expenses
 
 projected_annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
+print(f"Your monthly savings are ${monthly_savings:.2f}.")
+print(f"Projected savings after one year, with interest, is: ${projected_annual_savings:.2f}.")
