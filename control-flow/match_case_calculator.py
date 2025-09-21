4f8c981c98fa9cd01a8dbbd4db436f9ac0f2a392
Tony Abuya <tonyowuor22@gmail.com>
2025-09-21 18:25:11 +0300

    prompt for choosing operation

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,3 +3,8 @@
 # Prompt user for two numbers
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
+
+# Prompt for the operation
+operation = input("Choose the operation (+, -, *, /): ")
+
+
