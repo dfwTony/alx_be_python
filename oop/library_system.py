8a87447364e91c127a5c31e86d1c11a89b536517
Tony Abuya <tonyowuor22@gmail.com>
2025-10-11 19:33:20 +0300

    Create library_system.py and add functions

---
+++ oop/library_system.py
@@ -0,0 +1,48 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}"
+        
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Pages: {self.page_count}"
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            print(book)
+
+library = Library()
+
+book1 = Book("Pride and Prejudice", "Jane Austen")
+ebook1 = EBook("Snow Crash", "Neal Stephenson", 500)
+printbook1 = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
+
+library.add_book(book1)
+library.add_book(ebook1)
+library.add_book(printbook1)
+
+library.list_books()
