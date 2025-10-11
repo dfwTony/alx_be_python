5ff67bfaeb5bece850b4e6cbc6397d3646773ed9
Tony Abuya <tonyowuor22@gmail.com>
2025-10-11 18:33:20 +0300

    Updated the printing part

--- oop/book_class.py
+++ oop/book_class.py
@@ -15,10 +15,8 @@ class Book:
         print(f"Deleting {self.title}")
 
 
-# Create the object
-my_book = Book("1984", "George Orwell", 1949)
-
-# Print outputs
-print(my_book)        # __str__
-print(repr(my_book))  # __repr__
-del my_book           # __del__
+if __name__ == "__main__":
+    my_book = Book("1984", "George Orwell", 1949)
+    print(my_book)
+    print(repr(my_book))
+    del my_book
