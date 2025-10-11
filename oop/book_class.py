0d6b33f399948b66de367b7f7b7867d577d7d775
Tony Abuya <tonyowuor22@gmail.com>
2025-10-11 18:27:05 +0300

    Updated my __str__

--- oop/book_class.py
+++ oop/book_class.py
@@ -5,7 +5,7 @@ class Book:
         self.year = year
 
     def __str__(self):
-        return f"{self.title} by {self.author} published in {self.year}"
+        return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
        return f"Book('{self.title}', '{self.author}', {self.year})"
@@ -16,9 +16,7 @@ class Book:
 my_book = Book("1984", "George Orwell", 1949)
 
 print(my_book)
-
 print(repr(my_book))
-
 del my_book
 
 
